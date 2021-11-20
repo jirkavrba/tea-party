1d0027c0ed2d0bfe52aee07e419b1a4656f15fe2
Jiří Vrba <jiri@vrba.dev>
2021-11-20 23:45:23 +0100

    Add game module with type defining the game modes

---
+++ lib/tea_party/game.ex
@@ -0,0 +1,19 @@
+defmodule TeaParty.Game do
+  @typedoc """
+  Type of the game which sets the scoring mechanism used.
+  There are currently only the following three modes supported:
+
+  - Green tea - all that matters is speed, the fastest player receives 5 points,
+    the second fastest receives 3 points, the third receives 1 points and others
+    do not receive any points.
+
+  - Yellow tea - the goal is to submit a space-separated list of words. The player
+    with the largest number of valid words wins and receives 5 poinst. If there is a tie,
+    player who submitted their wordlist receives the points.
+
+  - Red tea - the goal is to submit the longest valid word possible. The player with
+    the longest word receives 5 points. If there is a tie, player who submitted its
+    word first receives the points.
+  """
+  @type mode :: :green_tea | :yellow_tea | :red_tea
+end
