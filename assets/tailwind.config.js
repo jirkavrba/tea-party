ef024b2c9e01d33b565a9ecb7a016abce8fc610b
Jiří Vrba <jiri@vrba.dev>
2021-11-21 23:07:32 +0100

    Wip username entry page design

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     mode: 'jit',
     purge: [
       './js/**/*.js',
       '../lib/*_web/**/*.*ex'
     ],
-    theme: {},
+    theme: {
+      extend: {
+        colors: {
+          gray: colors.trueGray,
+          green: colors.lime
+        }
+      }
+    },
     variants: {},
     plugins: []
 }
