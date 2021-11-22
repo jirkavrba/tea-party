af6fa919bc5da2b8267271d5daee239f68bc4cea
Jiří Vrba <jiri@vrba.dev>
2021-11-22 12:53:54 +0100

    Work in progress security configuration

---
+++ src/main/kotlin/dev/vrba/teaparty/security/WebSecurityConfiguration.kt
@@ -0,0 +1,23 @@
+package dev.vrba.teaparty.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                    .antMatchers("/api/login").permitAll()
+                    .antMatchers("/api/**").authenticated()
+    }
+}
