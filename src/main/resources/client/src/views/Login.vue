5a2428fa261dc271ba66d0dd75fb858a1d45c90a
Jiří Vrba <jiri@vrba.dev>
2021-11-22 11:33:12 +0100

    Formatting

--- src/main/resources/client/src/views/Login.vue
+++ src/main/resources/client/src/views/Login.vue
@@ -7,7 +7,9 @@
       focus:bg-white focus:outline-none hover:border-green-300 focus:ring-8 ring-green-400" v-model="username" placeholder="At least 3 characters">
 
       <button :disabled="disabled" :class="`w-full mt-10 text-3xl font-black pointer p-5 rounded-xl transition ${
-        disabled ? 'bg-green-400 text-green-300 cursor-not-allowed' : 'text-white bg-green-600 hover:bg-green-700 focus:bg-green-700 focus:outline-none cursor-pointer'
+        disabled
+        ? 'bg-green-400 text-green-300 cursor-not-allowed'
+        : 'text-white bg-green-600 hover:bg-green-700 focus:bg-green-700 focus:outline-none cursor-pointer'
       }`">Continue <i class="ml-5 text-2xl fas fa-arrow-right"></i></button>
     </div>
   </div>
