53d35be4b5e74815bcea444c377513f337b6273b
Jiří Vrba <jiri@vrba.dev>
2021-11-23 13:47:52 +0100

    Lobbies listing implementation

--- src/main/kotlin/dev/vrba/teaparty/controllers/LobbiesController.kt
+++ src/main/kotlin/dev/vrba/teaparty/controllers/LobbiesController.kt
@@ -8,23 +8,31 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 
 @Validated
 @RestController
-@RequestMapping("/api/lobby")
+@RequestMapping("/api")
 class LobbiesController(private val repository: LobbiesRepository) {
+
+    @GetMapping("/lobbies")
+    fun listLobbies(): ResponseEntity<*> {
+        val lobbies = repository.findAll().toList()
+        val response = mapOf(
+                "count" to lobbies.size,
+                "lobbies" to lobbies
+        )
+
+        return ResponseEntity.ok(response)
+    }
 
     // TODO: Add support for lobby configuration
     data class CreateLobbyRequest(@NotBlank val mode: String)
 
-    @PostMapping("/create")
+    @PostMapping("/lobby/create")
     fun createLobby(@AuthenticationPrincipal host: Player, @Valid @RequestBody request: CreateLobbyRequest): ResponseEntity<*> {
         val mode = when (request.mode) {
             "green" -> GameMode.GreenTea
