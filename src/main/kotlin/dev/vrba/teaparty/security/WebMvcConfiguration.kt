344026697011e0bbf209f1a0c435aabca406393e
Jiří Vrba <jiri@vrba.dev>
2021-11-23 10:15:06 +0100

    Make CORS mapping environment-dependent

--- src/main/kotlin/dev/vrba/teaparty/security/WebMvcConfiguration.kt
+++ src/main/kotlin/dev/vrba/teaparty/security/WebMvcConfiguration.kt
@@ -1,17 +1,22 @@
 package dev.vrba.teaparty.security
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class WebMvcConfiguration : WebMvcConfigurer {
-    // TODO: make this environment profiles dependent
+class WebMvcConfiguration(private val environment: Environment) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowCredentials(false)
+        // In production, the frontend package is built in CI and server from static resources,
+        // therefore they are served from the same domain as the API and there is no need for CORS mapping
+        if (environment.acceptsProfiles(Profiles.of("development"))) {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowCredentials(false)
+        }
     }
 }
