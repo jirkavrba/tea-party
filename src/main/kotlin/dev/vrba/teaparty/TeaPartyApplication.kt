11e64cf63a35f861c323c16cba4b833fa7e5cb7b
Jiří Vrba <jiri@vrba.dev>
2021-11-23 22:19:22 +0100

    Implement database seeder

--- src/main/kotlin/dev/vrba/teaparty/TeaPartyApplication.kt
+++ src/main/kotlin/dev/vrba/teaparty/TeaPartyApplication.kt
@@ -1,10 +1,24 @@
 package dev.vrba.teaparty
 
+import dev.vrba.teaparty.development.DatabaseSeeder
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
 
 @SpringBootApplication
-class TeaPartyApplication
+class TeaPartyApplication(
+        private val environment: Environment,
+        private val seeder: DatabaseSeeder
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        // TODO: Remove this later probably
+        if (environment.acceptsProfiles(Profiles.of("development"))) {
+            seeder.run()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<TeaPartyApplication>(*args)
---
+++ src/main/kotlin/dev/vrba/teaparty/development/DatabaseSeeder.kt
@@ -0,0 +1,22 @@
+package dev.vrba.teaparty.development
+
+import dev.vrba.teaparty.engine.GameMode
+import dev.vrba.teaparty.entities.Lobby
+import dev.vrba.teaparty.entities.Player
+import dev.vrba.teaparty.repositories.LobbiesRepository
+import dev.vrba.teaparty.repositories.PlayersRepository
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseSeeder(
+    private val playersRepository: PlayersRepository,
+    private val lobbiesRepository: LobbiesRepository
+) {
+    fun run() {
+        val player = playersRepository.save(Player(username = "Tea Party bot"))
+        val lobbies = listOf(GameMode.GreenTea, GameMode.GreenTea, GameMode.YellowTea, GameMode.RedTea, GameMode.YellowTea)
+                .map { Lobby(mode = it, host = player) }
+                .let { lobbiesRepository.saveAll(it) }
+
+    }
+}
