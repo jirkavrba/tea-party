911c2dbea2b1b693da13f2389ee3fe917a44c13e
Jiří Vrba <jiri@vrba.dev>
2021-11-23 15:49:35 +0100

    Add authentication check endpoint for validating tokens

--- src/main/kotlin/dev/vrba/teaparty/controllers/LoginController.kt
+++ src/main/kotlin/dev/vrba/teaparty/controllers/LoginController.kt
@@ -3,11 +3,9 @@ package dev.vrba.teaparty.controllers
 import dev.vrba.teaparty.entities.Player
 import dev.vrba.teaparty.repositories.PlayersRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
@@ -25,5 +23,12 @@ class LoginController(private val repository: PlayersRepository) {
         val player = Player(username = username)
 
         return ResponseEntity.ok(repository.save(player))
+    }
+
+    @GetMapping("/check")
+    fun check(@AuthenticationPrincipal player: Player): ResponseEntity<*> {
+        return ResponseEntity.ok(mapOf(
+            "message" to "Logged in as ${player.username}"
+        ))
     }
 }
