dc8375ce105cf88998a515dffa810776b4f08159
Jiří Vrba <jiri@vrba.dev>
2021-11-25 21:29:08 +0100

    Remove unused GamesRepository

--- src/main/kotlin/dev/vrba/teaparty/repositories/GamesRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.vrba.teaparty.repositories
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import java.util.*
-
-@Repository
-interface GamesRepository : CrudRepository<Game, UUID>
