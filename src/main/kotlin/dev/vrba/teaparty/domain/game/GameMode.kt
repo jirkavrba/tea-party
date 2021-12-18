dbca584f195c369fa070f04f3997c31184e4bc05
Jiří Vrba <jiri@vrba.dev>
2021-12-19 00:35:00 +0100

    Extract scoring type to a separate enum class

--- src/main/kotlin/dev/vrba/teaparty/domain/game/GameMode.kt
+++ src/main/kotlin/dev/vrba/teaparty/domain/game/GameMode.kt
@@ -1,10 +1,5 @@
 package dev.vrba.teaparty.domain.game
 
-enum class ScoringType {
-    Single,
-    TopThreeWords
-}
-
 enum class GameMode(scoring: ScoringType) {
     GreenTea(ScoringType.TopThreeWords),
     YellowTea(ScoringType.Single),
