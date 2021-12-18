0b7d5e1e63e6d4d393475af1c9d552aebc01c138
Jiří Vrba <jiri@vrba.dev>
2021-12-18 23:48:16 +0100

    Work in progress security configuration

---
+++ src/main/kotlin/dev/vrba/teaparty/security/SecurityConfiguration.kt
@@ -0,0 +1,29 @@
+package dev.vrba.teaparty.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+       http.cors().and()
+           .csrf().disable()
+           .authorizeRequests()
+           .antMatchers("/api/**").hasRole("PLAYER")
+           .antMatchers("/api/authentication/**").permitAll()
+           .anyRequest().permitAll().and()
+           .formLogin().disable()
+           .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication() // This will disable the built-in user account
+    }
+
+}
