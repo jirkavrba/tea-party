dd63ae38c62dd641106649b080152a7cebabcc8d
Jiří Vrba <jiri@vrba.dev>
2021-12-18 13:45:14 +0100

    Implement scoring words based on the selected game mode

--- src/main/kotlin/dev/vrba/teaparty/domain/game/GameMode.kt
+++ src/main/kotlin/dev/vrba/teaparty/domain/game/GameMode.kt
@@ -1,7 +1,12 @@
 package dev.vrba.teaparty.domain.game
 
-enum class GameMode {
-    GreenTea,
-    YellowTea,
-    RedTea
+enum class ScoringType {
+    Single,
+    TopThreeWords
+}
+
+enum class GameMode(scoring: ScoringType) {
+    GreenTea(ScoringType.TopThreeWords),
+    YellowTea(ScoringType.Single),
+    RedTea(ScoringType.Single)
 }
