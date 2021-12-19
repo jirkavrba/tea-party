bf8aa196fd89fd507f32f800f40aff75c44e213f
Jiří Vrba <jiri@vrba.dev>
2021-12-19 15:09:21 +0100

    App bar implementation

--- src/main/resources/client/src/App.vue
+++ src/main/resources/client/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <v-app>
+    <v-app-bar color="black">
+      <v-btn icon="mdi-github" href="https://github.com/jirkavrba/tea-party" target="_blank" color="transparent" text-color="white"></v-btn>
+      <v-spacer/>
+      <v-app-bar-title class="text-white">
+        <v-icon class="mr-4">mdi-tea</v-icon>
+        <strong>Tea party</strong>
+      </v-app-bar-title>
+      <v-spacer/>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
