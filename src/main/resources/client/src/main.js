718826102fbddc798789a15b275514a6fb4a2056
Jiří Vrba <jiri@vrba.dev>
2021-12-20 21:20:44 +0100

    Add authentication navigation guard to router

--- src/main/resources/client/src/main.js
+++ src/main/resources/client/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './router.js'
+import store from './store.js'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
--- src/main/resources/client/src/router.js
+++ src/main/resources/client/src/router.js
@@ -1,25 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
+import store from "./store";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  }
+    {
+        path: '/',
+        name: "Lobbies",
+        redirect: "/lobbies",
+    },
+    {
+        name: "Login",
+        path: "/login",
+        component: Login
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes,
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== "Login" && (store.state.token === null || store.state.token === "null")) {
+        next({name: "Login"});
+    }
+    else {
+        next();
+    }
 })
 
 export default router
