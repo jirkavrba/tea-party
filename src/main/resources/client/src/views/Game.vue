1f84541bd70635c7c2d03cb1d64663f26ccc8a7e
Jiří Vrba <jiri@vrba.dev>
2021-12-22 21:37:29 +0100

    Show syllable and remaining time in game component

--- src/main/resources/client/src/views/Game.vue
+++ src/main/resources/client/src/views/Game.vue
@@ -12,15 +12,21 @@
           <v-card class="d-flex flex-column" style="min-height: 90vh">
             <v-card-title>Game</v-card-title>
             <v-card-subtitle class="text-overline">
-              Playing
-              <span class="black--text">{{ game.mode.replace("T", " t") }}</span>
+              Playing <span class="black--text">{{ game.mode.replace("T", " t") }}</span>
             </v-card-subtitle>
+            <v-divider/>
+            <v-card-text>
+              <div class="text-overline">
+                <h2 class="grey--text">Type in a word containing <strong class="primary--text">SYL</strong></h2>
+              </div>
+              <v-progress-linear value="80" max="100"></v-progress-linear>
+            </v-card-text>
             <v-divider/>
             <v-card-text class="flex-grow-1">
             </v-card-text>
             <v-divider/>
             <v-card-text class="d-flex flex-row align-stretch">
-              <v-text-field outlined class="flex-grow-1"></v-text-field>
+              <v-text-field outlined class="flex-grow-1" hint="Tip: You can use enter to submit the word" :persistent-hint="true" autofocus></v-text-field>
               <v-btn color="primary" class="ml-2" icon x-large>
                 <v-icon>mdi-chevron-double-right</v-icon>
               </v-btn>
