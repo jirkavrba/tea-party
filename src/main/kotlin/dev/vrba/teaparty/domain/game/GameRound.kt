d6ceb3340698eb03c28e6273e0a140a45b585c7e
Jiří Vrba <jiri@vrba.dev>
2021-12-23 10:46:59 +0100

    Implement creating new game rounds

--- src/main/kotlin/dev/vrba/teaparty/domain/game/GameRound.kt
+++ src/main/kotlin/dev/vrba/teaparty/domain/game/GameRound.kt
@@ -5,5 +5,6 @@ import java.time.Instant
 data class GameRound(
     val end: Instant,
     val mode: GameMode,
+    val syllable: String,
     val words: List<ScoredWord>,
 )
--- src/main/kotlin/dev/vrba/teaparty/service/GamesService.kt
+++ src/main/kotlin/dev/vrba/teaparty/service/GamesService.kt
@@ -3,11 +3,14 @@ package dev.vrba.teaparty.service
 import dev.vrba.teaparty.domain.Game
 import dev.vrba.teaparty.domain.Lobby
 import dev.vrba.teaparty.domain.Player
+import dev.vrba.teaparty.domain.game.GameRound
 import dev.vrba.teaparty.exceptions.GameNotFoundException
 import dev.vrba.teaparty.exceptions.NotJoinedInGameException
 import dev.vrba.teaparty.repository.GamesRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.Instant
 import java.util.*
 
 @Service
@@ -16,18 +19,15 @@ class GamesService(private val repository: GamesRepository) {
     fun deleteAllGames() = repository.deleteAll()
 
     fun createGame(lobby: Lobby): Game {
-        return Game(
-            mode = lobby.mode,
-            players = lobby.players,
-            scores = lobby.players.associate { it.id to 0 }
-        ).let {
+        val scores = lobby.players.associate { it.id to 0 }
+
+        return Game(mode = lobby.mode, players = lobby.players, scores = scores).let {
             repository.save(it)
         }
     }
 
     fun findGame(id: UUID, player: Player): Game {
         // I can imagine this being handled with monads, maybe one day...
-
         val game = repository.findByIdOrNull(id) ?: throw GameNotFoundException
 
         if (!game.players.contains(player)) {
@@ -35,5 +35,14 @@ class GamesService(private val repository: GamesRepository) {
         }
 
         return game
+    }
+
+    fun createNewRound(game: Game): GameRound {
+        // TODO: Add proper list of syllables / procedural generator
+        val syllable = listOf("syl", "sys", "cot").random()
+        // TODO: Add game configuration later
+        val end = Instant.now() + Duration.ofSeconds(15)
+
+        return GameRound(end = end, mode = game.mode, syllable = syllable, listOf())
     }
 }
